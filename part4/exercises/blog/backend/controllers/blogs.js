8409bc3a7db6376b6281819956c1f2f815cb2ae3
Fakez <drzjamil@gmail.com>
2021-07-06 22:26:51 -0300

    finished part 4a

--- part4/exercises/blog/backend/controllers/blogs.js
+++ part4/exercises/blog/backend/controllers/blogs.js
@@ -1,51 +1,43 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-  Blog.find({}).then(blogs => {
-    response.json(blogs)
-  })
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({});
+  response.json(blogs);
 })
 
-blogsRouter.get('/:id', (request, response, next) => {
-  Blog.findById(request.params.id)
-    .then(blog => {
-      if (blog) {
-        response.json(blog)
-      } else {
-        response.status(404).end()
-      }
-    })
-    .catch(error => next(error))
+blogsRouter.get('/:id', async (request, response, next) => {
+  const blog = await Blog.findById(request.params.id)
+  if (blog) {
+    response.json(blog);
+  } else {
+    response.status(404).end();
+  }
+
 })
 
-blogsRouter.post('/', (request, response, next) => {
-  const body = request.body
-  console.log(body)
+blogsRouter.post('/',  async (request, response, next) => {
+  const body = request.body;
+  console.log(body);
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes,
-  })
+  });
 
-  blog.save()
-    .then(savedBlog => {
-      response.json(savedBlog)
-    })
-    .catch(error => next(error))
+  const savedBlog = await blog.save();
+  response.json(savedBlog);
+    
 })
 
-blogsRouter.delete('/:id', (request, response, next) => {
-  Blog.findByIdAndRemove(request.params.id)
-    .then(() => {
-      response.status(204).end()
-    })
-    .catch(error => next(error))
+blogsRouter.delete('/:id', async (request, response, next) => {
+  await Blog.findByIdAndRemove(request.params.id);
+  response.status(204).end();
 })
 
 blogsRouter.put('/:id', (request, response, next) => {
-  const body = request.body
+  const body = request.body;
 
   const blog = {
     title: body.title,
@@ -56,9 +48,9 @@ blogsRouter.put('/:id', (request, response, next) => {
 
   Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
     .then(updatedBlog => {
-      response.json(updatedBlog)
+      response.json(updatedBlog);
     })
     .catch(error => next(error))
 })
 
-module.exports = blogsRouter
+module.exports = blogsRouter;
