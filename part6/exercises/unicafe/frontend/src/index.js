37348739895b5d60c51876f60b478ff3223189c2
Fakez <drzjamil@gmail.com>
2021-07-11 23:32:41 -0300

    finished 6.2

--- part6/exercises/unicafe/frontend/src/index.js
+++ part6/exercises/unicafe/frontend/src/index.js
@@ -3,24 +3,49 @@ import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
 import reducer from './reducer'
 
-const store = createStore(reducer)
+const initialState = {
+  good: 0,
+  ok: 0,
+  bad: 0
+}
+
+const counterReducer = (state = initialState, action) => {
+  console.log(action)
+  switch (action.type) {
+    case 'GOOD':
+      return {...state, good: state.good += 1}
+    case 'OK':
+      return {...state, ok: state.ok += 1}
+    case 'BAD':
+      return {...state, bad: state.bad += 1}
+    case 'ZERO':
+      return {good: 0, ok: 0, bad: 0}
+    default:
+      return state;
+  }
+}
+
+const store = createStore(counterReducer)
 
 const App = () => {
-  const good = () => {
+
+  const dispatchToCounter = (actionType) => {
     store.dispatch({
-      type: 'GOOD'
+      type: actionType
     })
+  
   }
 
   return (
     <div>
-      <button onClick={good}>good</button> 
-      <button>neutral</button> 
-      <button>bad</button>
-      <button>reset stats</button>
+      <button onClick={() => dispatchToCounter('GOOD')}>good</button> 
+      <button onClick={() => dispatchToCounter('OK')}>neutral</button> 
+      <button onClick={() => dispatchToCounter('BAD')}>bad</button>
+      <button onClick={() => dispatchToCounter('ZERO')}>reset stats</button>
+
       <div>good {store.getState().good}</div>
-      <div>neutral</div>
-      <div>bad</div>
+      <div>neutral {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
     </div>
   )
 }
