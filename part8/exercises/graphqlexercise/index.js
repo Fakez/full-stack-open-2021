35ed1c46817702c7360ef1ae8895f22f9ccfca75
Fakez <drzjamil@gmail.com>
2021-07-14 23:08:21 -0300

    finished 8.5

--- part8/exercises/graphqlexercise/index.js
+++ part8/exercises/graphqlexercise/index.js
@@ -97,7 +97,7 @@ const typeDefs = gql`
     type Query {
         bookCount: Int!
         authorCount: Int!
-        allBooks(author: String!): [Book!]!
+        allBooks(author: String, genre:String): [Book!]!
         allAuthors: [Author]
     }
 `
@@ -105,11 +105,22 @@ const typeDefs = gql`
 const resolvers = {
   Query: {
       bookCount: () => books.length,
+
       authorCount: () => authors.length,
+
       allBooks: (root, args) => {
-        if (!args.author) return books;
-        return books.filter(b => b.author === args.author)
+        if (!args.author && !args.genre) return books;
+
+        if (args.author && !args.genre) {
+            return books.filter(b => b.author === args.author);
+        }
+        if (!args.author && args.genre) {
+            return books.filter(b => b.genres.includes(args.genre));
+        }
+        return books.filter(b => b.author === args.author 
+            && b.genres.includes(args.genre));
       },
+
       allAuthors: () => authors.map(({name}) => {
         const bookCount = books.filter(b => b.author === name).length;
         return { name, bookCount }
