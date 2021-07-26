223a57eec618e0b4a640ae8c275be1d60ddabcd9
Fakez <drzjamil@gmail.com>
2021-07-25 22:02:37 -0300

    finished calculator from tutorial

---
+++ part9/exercises/ts-ex/tutorial.ts
@@ -0,0 +1,22 @@
+// const multiplicator = (a: number, b: number, printText: string) => {
+//   console.log(printText,  a * b);
+// }
+  
+// multiplicator(2, 4, 'Multiplied a string and 4, the result is:');
+
+type Operation = 'multiply' | 'add' | 'divide';
+type Result = string | number | undefined
+
+const calculator = (a: number, b: number, op: Operation): Result => {
+
+  if (op === 'multiply') {
+    return a * b;
+  } else if (op === 'add') {
+    return a + b;
+  } else if (op === 'divide') {
+    if (b === 0) return 'this cannot be done';
+    return a / b;
+  }
+}
+
+console.log(calculator(2, 2, 'multiply'))
