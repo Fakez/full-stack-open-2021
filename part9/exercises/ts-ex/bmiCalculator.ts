0c0b9b11a42ba2d2fcc7d26e8a612cb89dba3c74
Fakez <drzjamil@gmail.com>
2021-07-25 22:47:56 -0300

    finished 9.4 and 9.5

--- part9/exercises/ts-ex/bmiCalculator.ts
+++ part9/exercises/ts-ex/bmiCalculator.ts
@@ -2,4 +2,4 @@ const calculateBmi = (heightCm: number, weightKg: number): number => {
     return weightKg / Math.pow(heightCm / 100, 2)
 }
 
-console.log(calculateBmi(180, 74))
+module.exports = {calculateBmi};
--- part9/exercises/ts-ex/index.ts
+++ part9/exercises/ts-ex/index.ts
@@ -1,8 +1,25 @@
 import express from 'express';
+const {calculateBmi} = require('./bmiCalculator')
 const app = express();
 
 app.get('/ping', (_req, res) => {
   res.send('pong');
+});
+
+app.get('/hello', (_req, res) => {
+    res.send('Hello Full Stack!')
+});
+
+app.get('/bmi', (req, res) => {
+    const weight = req.query.weight
+    const height = req.query.height
+
+    const response = {
+        weight: weight,
+        height: height,
+        bmi: calculateBmi(weight, height)
+    }
+    res.json(response)
 });
 
 const PORT = 3003;
