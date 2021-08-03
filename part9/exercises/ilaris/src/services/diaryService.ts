81bd5f8b09dc000e2ca7d86c50b279654825ac81
Fakez <drzjamil@gmail.com>
2021-08-02 21:44:00 -0300

    added types and folder structure

---
+++ part9/exercises/ilaris/src/services/diaryService.ts
@@ -0,0 +1,15 @@
+
+import diaries from '../data/diaries'
+
+const getEntries = () => {
+  return diaries;
+};
+
+const addEntry = () => {
+  return null;
+};
+
+export default {
+  getEntries,
+  addEntry
+};
