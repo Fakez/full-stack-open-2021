8c9ee635b68c9960960f6a6773311c7743d0176b
Fakez <drzjamil@gmail.com>
2021-08-02 22:02:25 -0300

    finished utility types

--- part9/exercises/ilaris/src/services/diaryService.ts
+++ part9/exercises/ilaris/src/services/diaryService.ts
@@ -1,15 +1,28 @@
 
 import diaries from '../data/diaries'
+import { NonSensitiveDiaryEntry } from '../types';
+
 
 const getEntries = () => {
   return diaries;
+};
+
+const getNonSensitiveEntries = (): NonSensitiveDiaryEntry[] => {
+  return diaries.map(({ id, date, weather, visibility }) => ({
+    id,
+    date,
+    weather,
+    visibility,
+  }));
 };
 
 const addEntry = () => {
   return null;
 };
 
+
 export default {
   getEntries,
+  getNonSensitiveEntries,
   addEntry
 };
